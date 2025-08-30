12cb87c2b4767350d1bf4c0702c527838eaadc20
sabogal <canoas430@gmail.com>
2025-08-29 20:06:25 -0500

    feat: convert temperature

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+from .temperature import Temperature
+
+temperature_type = input("Ingrese el tipo de temperatura Farenheit (1) o Celcius (2): ")
+value = input("Ingrese la temperatura: ")
+
+temperature_instance = Temperature(temperature=value, temperature_type=temperature_type)
+print(temperature_instance.get_temperature())
